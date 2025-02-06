3317cb299417a1780afe4c091f4a9a4ce31535f3
THINK BOOK <dhairya.jangir.s73@kalvium.community>
2025-02-06 16:57:02 +0530

    assignment2 done

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 const express = require('express');
 const { resolve } = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const { type } = require('os');
+require('dotenv').config();
 
 const app = express();
-const port = 3010;
+const port = 3010 || process.env.PORT;
 
+app.use(bodyParser.json());
 app.use(express.static('static'));
+
+mongoose.connect(process.env.MONGO_URL ||  "mongodb+srv://dhairyajangirs73:dhairya123@cluster0.fe8x2.mongodb.net/", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => { console.log('Connected to MongoDB'); })
+  .catch((err) => { console.error(err); });
+
+// define menuItems schema
+const menuItemsSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  price: {type: Number, required: true},
+  description: String,
+  price: {type: Number, required: true},
+});
+
+const menuItemsModel = mongoose.model('menuItems', menuItemsSchema);
+
+// update menu item
+app.put('/menuItems/:id', async (req, res) => {
+  try {
+    const {name, price, description} = req.body;
+    const updatedMenuItem = await menuItemsModel.findByIdAndUpdate(req.params.id, {name, price, description}, {new: true});
+    res.json(updatedMenuItem);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
